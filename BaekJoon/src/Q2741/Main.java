c874740be2b194df939e08416fbbf1f99ffae183
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-12-30 14:47:14 +0900

    2nd commit

--- BaekJoon/src/Q2741/Main.java
+++ BaekJoon/src/Q2741/Main.java
@@ -1,12 +1,20 @@
 package Q2741;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int N = 10; 
-        
-        for (int i = 1; i <= N; i++) {
-            System.out.println(i); 
-        }
+       Scanner sc = new Scanner(System.in); // scanner 선언 시 끝에 close()메서드로 종료 필요
+       int N = sc.nextInt();
+       
+       sc.close();
+
+       
+       for(int i = 1; i<=N; i++ ) {
+    	   System.out.println(i);
+    	   
+       }
+       
     }
 }
 
