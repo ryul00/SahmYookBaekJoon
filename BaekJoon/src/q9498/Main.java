d20a693bafc51198f7de25ab5fffaded8a5d7ffc
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-12-30 16:45:27 +0900

    9498번 if/switch-case 풀이

--- BaekJoon/src/q9498/Main.java
+++ BaekJoon/src/q9498/Main.java
@@ -10,18 +10,43 @@ public class Main {
 	       
 	       sc.close();
 
-	       
-	       if (score >= 90) {
+	       // if문
+	       if (90 <= score && score <= 100) {
 	    	    System.out.println("A");
-	    	} else if (score >= 80) {
+	    	} else if (80 <= score && score <= 89) {
 	    	    System.out.println("B");
-	    	} else if (score >= 70) {
+	    	} else if (70 <= score && score <= 79) {
 	    	    System.out.println("C");
-	    	} else if (score >= 60) {
+	    	} else if (60 <= score && score <= 69) {
 	    	    System.out.println("D");
 	    	} else {
 	    	    System.out.println("F");
 	    	}
+	       
+	       
+	       // switch-case문
+	       switch(score) {
+	       case 90,91,92,93,94,95,96,97,98,99,100:
+	    	   System.out.println('A');
+	    	   break;
+	      
+	       case 80,81,82,83,84,85,86,87,88,89:
+	    	   System.out.println('B');
+	       	   break;
+	       	   
+	       case 70,71,72,73,74,75,76,77,78,79:
+	    	   System.out.println('c');
+	       	   break;
+	       	   
+	       case 60,61,62,63,64,65,66,67,68,69:
+	    	   System.out.println('d');
+	       	   break;
+	       	   
+	       default:
+	    	   System.out.println('F'); 
+	       }
+	       
+	       
 
 	}
 
