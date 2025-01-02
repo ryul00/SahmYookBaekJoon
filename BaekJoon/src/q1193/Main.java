e328eb5844d1514f7b960d8649d3a54a245c97c1
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-03 01:56:44 +0900

    1193 : 강사님 풀이 1193_2 : 내가 참고하여 풀어본 것

--- BaekJoon/src/q1193/Main.java
+++ BaekJoon/src/q1193/Main.java
@@ -3,7 +3,7 @@ package q1193;
 import java.util.Scanner;
 
 public class Main {
-	private static int leftValue = 1, rightValue = 1;
+	//private static int leftValue = 1, rightValue = 1;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -14,7 +14,8 @@ public class Main {
 		// LEFTDOWN - DOWN
 		// 1/1 - 1/2 - 2/1 - 3/1 - 2/2 - 1/3 - 1/4 - 2/3 - 3/2 - 4/1 - 5/1
 		// RIGHT(1) - LEFTDOWN(1, 3, 5 ...) - DOWN(1) - RIGHTUP(2, 4, 6 ...) - RIGHT
-
+		int rightValue = 1;
+		int leftValue = 1;
 		int ldcnt = 1;
 		int rucnt = 2;
 		while (true) {
---
+++ BaekJoon/src/q1193_2/Main.java
@@ -0,0 +1,69 @@
+package q1193_2;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		sc.close();
+		
+		int x = 1;
+		int y = 1;
+		
+		int ldc = 1;
+		int ruc = 2;
+		
+		// 헷갈리는 부분 : 종료 조건
+		// for문 내부 : ld,ru 반복 수행 시 X==0이되면 for문 탈출 
+		// for문 외부 : for문 탈출 후 X==0 확인 후 While문 까지 탈출 
+		// 결론 : 각 단계 수행 전 X==0 확인 + for,while은 독립적이므로 for탈출해도 while자체를 탈출한 건 아님!
+		// Down,Right전에 X==0을 확인 하는 것은 수행전에 X==0이 되면 안되기 때문이다.
+		while(true) {
+			if(--X == 0) {
+				break;
+			}
+			// Right
+			y++;
+			
+			// LeftDown
+			for(int i=0;i<ldc;i++) {
+				if(--X == 0) {
+					break;
+				}
+				x++;
+				y--;
+			}
+			if(X==0) {
+				break;
+			}
+			ldc += 2;
+			
+			if(--X == 0) {
+				break;
+			}
+			// Down
+			x++;
+			
+			// RightUp
+			for(int j=0; j<ruc;j++) {
+				if(--X==0) {
+					break;
+				}
+				x--;
+				y++;
+			}
+			if(X==0) {
+				break;
+			}
+			ruc += 2;
+			
+		}
+		System.out.println(x + "/" + y);
+		
+		
+	}
+
+}
