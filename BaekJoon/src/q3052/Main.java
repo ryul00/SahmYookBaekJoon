7ed7e1c4d1e8feac6ba951f813ff7fc89e038fc6
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-21 13:49:01 +0900

    q3052 distict()배열 내 중복제거 메서드 기억하기

---
+++ BaekJoon/src/q3052/Main.java
@@ -0,0 +1,34 @@
+package q3052;
+
+import java.beans.JavaBean;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[10];
+		int arr2[] = new int[10];
+		for(int i=0; i<arr.length; i++) {
+			arr[i]= sc.nextInt();
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			arr2[i] = arr[i] % 42; 
+		}
+		
+		arr2 = Arrays.stream(arr2).distinct().toArray();
+		
+		System.out.println(arr2.length);
+		
+		
+		
+		
+		
+
+
+
+	}
+
+}
