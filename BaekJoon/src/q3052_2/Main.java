67342b0b876f1df47a696eefe0836835548f1bd2
ryul00 <gju07059@gmail.com>
2025-02-14 09:43:10 +0900

    q3052 Hashset

--- BaekJoon/src/q3052_2/Main.java
+++ BaekJoon/src/q3052_2/Main.java
@@ -1,28 +1,27 @@
 package q3052_2;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int arr[] = new int[10];
-		int arr2[] = new int[10];
 		for(int i=0; i<arr.length; i++) {
 			arr[i] = sc.nextInt();
 			
 		}
 		
-		for (int i=0; i<arr.length; i++) {
-			arr2[i] = arr[i] % 42;
+		HashSet<Integer> intp = new HashSet<Integer>();
+		
+		for(int i=0; i< arr.length; i++) {
+			intp.add(arr[i] % 42);
 		}
-		sc.close();
 		
-		arr2 = Arrays.stream(arr2).distinct().toArray(); // 배열 중복 요소 제거
-		
-		System.out.println(arr2.length);
-
+		System.out.println(intp.size());
 	}
 
 }
