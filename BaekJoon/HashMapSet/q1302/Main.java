8b7d5253ff531e56982815b793007e2d87280bfd
ryul00 <gju07059@gmail.com>
2025-04-03 09:19:19 +0900

    map->set 변환 중요

--- BaekJoon/HashMapSet/q1302/Main.java
+++ BaekJoon/HashMapSet/q1302/Main.java
@@ -1,41 +1,43 @@
 package q1302;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		HashSet<Integer>Set_A = new HashSet<>();
-		HashSet<Integer>Set_B = new HashSet<>();
-		HashSet<Integer>Set_C = new HashSet<>();
-		for(int i=0; i<a; i++) {
-			int countA = sc.nextInt();
-			Set_A.add(countA);
-		}
+		int N = sc.nextInt();
+		HashMap<String, Integer> map = new HashMap<>();
 		
-		for(int i=0; i<b; i++) {
-			int countB = sc.nextInt();
-			Set_B.add(countB);
+		//입력
+		for(int i=0; i<N; i++) {
+			String title = sc.next();
+			map.put(title, map.getOrDefault(title, 0) + 1);
 		}
-		
 		sc.close();
 		
-		for(Integer elementB : Set_B) {
-			if(Set_A.contains(elementB)) {
-				Set_A.remove(elementB); // 1만남음
-			}else {
-				Set_A.add(elementB);
+		String FeqTitle = "";
+		int maxCount = 0;
+		
+		// 기존 해시맵인 map을 Set형태(entrySet)로 변환하여 개별 키,값을 분리하여 사용할 수 있음
+		for(Map.Entry<String, Integer> entry : map.entrySet()) {
+			int count = entry.getValue();
+			String key = entry.getKey();
+			
+			if(count > maxCount) {
+				maxCount = count;
+				FeqTitle = key;
+			}else if (count == maxCount) {
+				// key가 기존 FeqTitle보다 사전순으로 더 크면 음수 반환, 해당 key를 FeqTitle로 대체
+				if(key.compareTo(FeqTitle) < 0) {
+					FeqTitle = key;
+				}
 			}
 		}
+		System.out.println(FeqTitle);
 		
-
-		
-		System.out.println(Set_A.size());
-
 	}
 
 }
